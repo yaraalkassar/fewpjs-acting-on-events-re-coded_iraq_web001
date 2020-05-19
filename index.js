f9465efdbde973770b556dcbe372d2266cde6652
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-19 12:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
 
   if (right > 0) {
-    dodger.style.left = `${left + 1}px`;
+    dodger.style.right = `${left + 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
